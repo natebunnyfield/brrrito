7355bedbfa8e548131ed1bec9d35981c0cb28a7c
Nate Bunnyfield <nate@bunnyfields.com>
2013-01-12 23:57:13 -0600

    Initial draft that actually meets expectations

--- forecast.js
+++ forecast.js
@@ -5,58 +5,27 @@ var http = require('http')
       , city: 'Chicago'
       , state: 'IL'
     }
-  , http_options_dailies = {
-        host: api.host
-      , path: ['/api/', api.key, '/forecast10day/q/', api.state, '/', api.city, '.json'].join("")
-    }
-  , http_options_hourlies = {
+  , http_options = {
         host: api.host
       , path: ['/api/', api.key, '/hourly10day/q/', api.state, '/', api.city, '.json'].join("")
     }
   , forecast = {
         epoch: 0
-      , hourlies: []
+      , temps: []
       , unit: 60*60*1000 // milliseconds
     }
 
 // initialize forecast for the next 6 days
 forecast.epoch = new Date()
 forecast.epoch.setHours(0, 0, 0, 0)
-for (var h = 0; h < 24*12; h++) {
-  //forecast.hourlies.push(new Date(value=forecast.epoch.getTime()+(h*forecast.unit)))
-  forecast.hourlies.push(null)
+for (var h = 0; h < 24*11; h++) {
+  //forecast.temps.push(new Date(value=forecast.epoch.getTime()+(h*forecast.unit)))
+  forecast.temps.push(undefined)
 }
 
 function windchill(t, v) {
   // http://www.nws.noaa.gov/os/windchill/
   return Math.round(35.74+0.6215*t-35.75*Math.pow(v, 0.16)+0.4275*t*Math.pow(v, 0.16))
-}
-
-parse_dailies = function(response) {
-
-  function simplify(o) {
-    return {
-        date: new Date(o.date.epoch*1000)
-      , high: windchill(o.high.fahrenheit, o.avewind.mph)
-      , low: windchill(o.low.fahrenheit, o.avewind.mph)
-    }
-  }
-
-  var str = ''
-  response.on('data', function(chunk) {
-    str += chunk
-  })
-  response.on('end', function() {
-    dailies = JSON.parse(str).forecast.simpleforecast.forecastday.map(simplify)
-    for (var a in dailies) {
-      high_time = low_time = dailies[a].date
-      low_time.setHours(5, 0, 0, 0)
-      forecast.hourlies[(low_time-forecast.epoch)/forecast.unit] = dailies[a].low
-      high_time.setHours(16, 0, 0, 0)
-      forecast.hourlies[(high_time-forecast.epoch)/forecast.unit] = dailies[a].high
-    }
-    http.request(http_options_hourlies, parse_hourlies).end()
-  })
 }
 
 parse_hourlies = function(response) {
@@ -75,7 +44,7 @@ parse_hourlies = function(response) {
   response.on('end', function() {
     hourlies = JSON.parse(str).hourly_forecast.map(simplify)
     for (var a in hourlies) {
-      forecast.hourlies[(hourlies[a].time-forecast.epoch)/forecast.unit] = hourlies[a].temp
+      forecast.temps[(hourlies[a].time-forecast.epoch)/forecast.unit] = hourlies[a].temp
     }
     interpolate_forecast()
   })
@@ -85,4 +54,4 @@ interpolate_forecast = function() {
   console.log(forecast)
 }
 
-http.request(http_options_dailies, parse_dailies).end()
+http.request(http_options, parse_hourlies).end()
